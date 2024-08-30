bbf314cad8327393191b4473840126a7adb81721
Davud <davud@MacBook-Pro-Davud.local>
2024-08-30 19:42:25 +0500

    add_Update_Parents_Completion

--- src/components/taskItem/TaskItem.tsx
+++ src/components/taskItem/TaskItem.tsx
@@ -1,5 +1,5 @@
 import { observer } from "mobx-react-lite";
-import { Task } from "../taskStore/TaskStore";
+import TaskStore, { Task } from "../taskStore/TaskStore";
 import Checkbox from "@mui/material/Checkbox";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
@@ -9,43 +9,44 @@ import Collapse from "@mui/material/Collapse";
 import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
+
 interface TaskItemProps {
   task: Task;
   level?: number;
 }
 
-
 const TaskItem = observer(({ task, level = 0 }: TaskItemProps) => {
-  
   // Обработчик для переключения состояния "развернуто/свернуто" задачи
   const handleToggleExpand = () => {
-    
+    TaskStore.setIsExpanded(task.id);
   };
 
   return (
     <List disablePadding>
-
       {/* Кнопка для развертывания/свертывания подзадач */}
-      <ListItem sx={{ pl: level * 5 }}> {/* Отступ увеличивается с каждым уровнем вложенности */}
+      <ListItem sx={{ pl: level * 5 }}>
+        {" "}
+        {/* Отступ увеличивается с каждым уровнем вложенности */}
         {task.subtasks.length > 0 && (
           <IconButton onClick={handleToggleExpand}>
             {/* Иконка зависит от состояния развернутости задачи */}
             {task.isExpanded ? <ExpandLessIcon /> : <ExpandMoreIcon />}
           </IconButton>
         )}
-
         {/* Текст задачи */}
         <ListItemText
           sx={{ pl: 1 }} // Отступ между иконкой и текстом
           primary={task.title}
-          // onClick={}
+          onClick={()=>console.log(task.title)}
         />
-
+        <IconButton aria-label="delete">
+  <DeleteIcon />
+</IconButton>
         {/* Чекбокс для отметки завершенности задачи */}
         <Checkbox
           indeterminate={!task.isComplete}
           checked={task.isComplete}
-          // onChange={}
+          onChange={()=>TaskStore.toggleTaskCompletion(task.id)}
         />
       </ListItem>
 
--- src/components/taskStore/TaskStore.ts
+++ src/components/taskStore/TaskStore.ts
@@ -27,6 +27,15 @@ export class Task {
      что позволяет автоматически отслеживать изменения. */
     makeAutoObservable(this);
   }
+
+  toggleCompletion() {
+    this.isComplete = !this.isComplete;
+    this.subtasks.forEach(subtask => subtask.updateCompletion(this.isComplete));
+  }
+  updateCompletion(status: boolean) {
+    this.isComplete = status;
+    this.subtasks.forEach(subtask => subtask.updateCompletion(status));
+  }
 }
 
 class TaskStore {
@@ -42,21 +51,32 @@ class TaskStore {
 
   // Метод загрузки задач из JSON файла и сохранение их в localStorage.
   loadTasksFromFile() {
-    fetch("/tasks.json")
-      .then((response) => response.json())
-      .then((data) => {
-        // Сохранение загруженных данных задач в localStorage
-        localStorage.setItem("tasksData", JSON.stringify(data));
-        const tasksData = localStorage.getItem("tasksData");
-        if (tasksData) {
-          const parsedTasks = JSON.parse(tasksData);
-          this.tasks = parsedTasks.map((taskData: any) =>
-            this.createTask(taskData)
-          );
-        }
+    const tasksData = localStorage.getItem("tasksData");
+    if(tasksData){
+      this.parseTaskData(tasksData)
+    } else {
+      console.log('Из файла')
+      fetch("/tasks.json")
+        .then((response) => response.text())
+        .then((data) => {
+          // Сохранение загруженных данных задач в localStorage
+          localStorage.setItem("tasksData", data);
+
+          this.parseTaskData(data)
       })
       .catch((error) => console.error("Ошибка при загрузке задач:", error));
+    }
+    
   }
+
+  parseTaskData(tasksData:string){
+    console.log('Из localStorage')
+      const parsedTasks = JSON.parse(tasksData);
+      this.tasks = parsedTasks.map((taskData: any) =>
+        this.createTask(taskData)
+      );
+  }
+
 
   // Метод создания объекта Task на основе данных
   createTask(taskData: any): Task {
@@ -73,6 +93,45 @@ class TaskStore {
       subtasks ? subtasks.map((subtask: any) => this.createTask(subtask)) : []
     );
   }
+
+  setIsExpanded(id: string) {
+    const task = this.findTaskById(id);
+    if (task) {
+      task.isExpanded = !task.isExpanded;
+    }
+  }
+
+  toggleTaskCompletion(id: string) {
+    const task = this.findTaskById(id);
+    if (task) {
+      task.toggleCompletion();
+    }
+    this.updateParentsCompletion(id)
+  }
+
+  updateParentsCompletion (id:string){
+    let parent = this.findParent(id,this.tasks)
+    while (parent) {
+      parent.isComplete = parent?.subtasks.every(subtask => subtask.isComplete)
+      parent = this.findParent(parent.id, this.tasks)
+    }
+  }
+
+
+
+  findParent(id:string,tasksToCheck:Task[]):Task|undefined{
+    for (let task of tasksToCheck) {
+      if(task.subtasks?.some(subtask => subtask.id===id)){
+          return task
+      }
+  
+      const result = this.findParent(id, task.subtasks)
+      if(result) {
+          return result
+      }
+    }
+    
+  };
 
   // Метод поиска задачи по уникальному идентификатору в списке задач и их подзадачах
   findTaskById(id: string): Task | null {
